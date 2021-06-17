48e5b8b65eef1fc892ba16453959c272a20206bc
sara-arredondo <sara.arredondo.giraldo@gmail.com>
2021-06-17 23:38:26 +0000

    0x00_4_5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-/*definicion main*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  
-puts("\"Programming is like building a multilingual puzzle"); /*function to print a string*/
-return (0);
-} 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
