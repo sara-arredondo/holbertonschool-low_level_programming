74fa1240f4b5a7923c03c4b0ef0746a779a41ec5
sara-arredondo <sara.arredondo.giraldo@gmail.com>
2021-06-17 22:51:08 +0000

    0x00_4_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
 int main(void)
 {
   
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle"); /*function to print a string*/
+return (0);
 } 
