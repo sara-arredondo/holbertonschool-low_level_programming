f62fb163905c2d87d7eb189fd906eede9961b657
sara-arredondo <sara.arredondo.giraldo@gmail.com>
2021-06-18 03:01:37 +0000

    101_2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (success)
+ * Return: Always 1 (Success)
  */
-{
+
 int main(void)
+{
 	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19"\n");
 	return (1);
 }
+
