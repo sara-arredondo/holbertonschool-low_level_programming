d24e213d8ea0725a440ca9cc3bb835643ffdd351
sara-arredondo <sara.arredondo.giraldo@gmail.com>
2021-06-18 15:51:46 -0700

    0x01_7_1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int letter = 122;
+
+	while (letter >= 97)
+	{
+
+	putchar(letter);
+	letter--;
+}
+putchar('\n');
+return (0);
+}
