d7e7a608cb90a9de85c68d9dbffdc5a2a6bf07a3
sara-arredondo <sara.arredondo.giraldo@gmail.com>
2021-06-19 08:23:15 -0700

    0x01_5_1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
