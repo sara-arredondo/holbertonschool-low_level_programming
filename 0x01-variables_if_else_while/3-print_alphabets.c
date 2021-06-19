dd5d16b541c25f1c302ddd423f4c2ee685209674
sara-arredondo <sara.arredondo.giraldo@gmail.com>
2021-06-18 21:30:51 -0700

    0x01_3_3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int letter = 97;
+
+	while (letter <= 122)
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	int letter = 41;
+
+	while (letter <= 90)
+	{
+		putchar(letter);
+		letter++
+	}
+	putchar('\n');
+	return (0);
+}
