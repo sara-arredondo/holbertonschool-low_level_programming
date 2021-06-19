9c26bd089dff6873ab4b922c0870f4c47375f07f
sara-arredondo <sara.arredondo.giraldo@gmail.com>
2021-06-19 11:48:49 -0700

    0x01_8_1

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,9 +6,19 @@
  * Return: Always 0 (Sucess)
  *
  */
-int main (void)
+int main(void)
 {
 	int i;
+	int j;
 
-	for 
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97 ; <= 102 ; i++)
+	{
+		putchar(j);
+	}
+putchar('\n');
+return (0);
 }
