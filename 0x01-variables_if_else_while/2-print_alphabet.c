a1de9e86ebd78c4e3470189b6b9e023efaf35a69
sara-arredondo <sara.arredondo.giraldo@gmail.com>
2021-06-18 20:45:41 -0700

    0x01_2_1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 9 (Sucess)
+ */
+int main(void)
+{
+	int letter = 12i;
+
+	while (letter >= 97)
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
+
