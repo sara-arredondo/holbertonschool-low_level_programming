c428a937312b0ffb508a5556f8b06e8177106cec
sara-arredondo <sara.arredondo.giraldo@gmail.com>
2021-06-22 19:20:43 -0500

    0x02_2_11

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lowercase x10
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+int l = 97;
+while (l <= 122)
+{
+_putchar(l);
+l++;
+}
+}
+_putchar('\n');
+}
